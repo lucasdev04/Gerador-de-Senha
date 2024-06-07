b67b8e2a5bfcbfa8e1a0a0481675b3d551a9106f
Lucas Oliveira Ramalho <ramalhol656@gmail.com>
2024-06-07 17:39:32 -0300

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,25 @@
+const URL = "https://www.psswrd.net/api/v1/password/";
+const btnGerar = document.getElementById("btn-gerar");
+const inputSenha = document.getElementById("input-senha");
+const btnCopiar = document.getElementById("btn-copiar");
+
+btnGerar.addEventListener("click", GerarSenha);
+
+async function GerarSenha () {
+    const resp = await fetch(URL);
+    const obj = await resp.json();
+
+    inputSenha.value = obj.password;
+}
+
+btnCopiar.addEventListener("click", function () {
+    const senhaGerada = inputSenha.value;
+    inputSenha.select();
+    navigator.clipboard.writeText(senhaGerada);
+    btnCopiar.textContent = "Copiado!";
+    btnCopiar.classList.add("btn-copiado");
+    setTimeout(() => {
+        btnCopiar.textContent = "Copiar";
+        btnCopiar.classList.remove("btn-copiado");
+    }, 2000);
+});
